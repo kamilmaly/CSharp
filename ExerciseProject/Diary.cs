d224a604539bee36777bfd1a35584ca0248aa31f
Maly <Maly@KAMIL>
2020-01-21 21:51:07 +0100

    Add new project ExerciseProject

---
+++ ExerciseProject/Diary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseProject
+{
+    class Diary
+    {
+
+        public Diary()
+        {
+            ratings = new List<float>();
+        }
+
+        //Fields 
+        List<float> ratings;
+
+
+        //Methods
+        public void AddRating(float rating)
+        {
+            ratings.Add(rating);
+        }
+
+        public float CalculateAverage()
+        {
+            float sum = 0, avg = 0;
+            foreach (var rating in ratings)
+            {
+                sum += rating;
+            }
+            avg = sum / ratings.Count();
+            return avg;
+        }
+
+        public float GiveMaxRating()
+        {
+            return ratings.Max();
+        }
+
+        public float GiveMinRating()
+        {
+            return ratings.Min();
+        }
+    }
+}
---
+++ ExerciseProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diary diary = new Diary();
+            diary.AddRating(3);
+            diary.AddRating(5);
+
+            Diary diary2 = diary;
+            diary2.AddRating(8);
+
+            Diary diary3 = diary2;
+            diary3.AddRating(1);
+
+        }
+    }
+}
